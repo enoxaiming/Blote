a22504370094f828c40cff3bed3a66fd07487046
HaJunHyeok <enoxaiming@naver.com>
2017-08-06 23:55:52 +0900

    Add create view

---
+++ src/app/create/create.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { DomSanitizer,SafeHtml } from '@angular/platform-browser'
+
+declare const $: any;
+
+@Component({
+  selector: 'app-create',
+  templateUrl: './create.component.html',
+  styleUrls: ['./create.component.css']
+})
+export class CreateComponent implements OnInit {
+
+  _htmlText: string = "";
+
+  constructor(private sanitizer: DomSanitizer) { }
+
+  ngOnInit() {
+
+  }
+
+  public get htmlText() : SafeHtml {
+    return this.sanitizer.bypassSecurityTrustHtml(this._htmlText);
+  }
+
+  addRow() {
+    this._htmlText = this._htmlText + `<input type="text" class="form-control input-lg" name="name" id="votename-input" placeholder="투표 보기 입력">`;
+    console.log(this._htmlText.length);
+  }
+
+  deleteRow() {
+    this._htmlText = this._htmlText.slice(0,this._htmlText.length-104);
+    console.log(this._htmlText.length);
+  }
+
+}
