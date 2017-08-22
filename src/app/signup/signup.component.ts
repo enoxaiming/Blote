87a5539f4a88b4a1066fbb5a61a5a68c34c4bbb9
HaJunHyeok <enoxaiming@naver.com>
2017-08-22 22:09:25 +0900

    About Component

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
 import {AuthService} from '../service/auth.service';
+
+import { fadeInAnimation } from '../_animation/index';
+
  
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
+  animations: [fadeInAnimation],
+  host:{'[@fadeInAnimation':''}
 })
 export class SignupComponent implements OnInit {
 
